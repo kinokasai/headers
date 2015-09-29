d7d3519fa725ecf4920cef689599a45498c3d738
Alexandre Doussot <dousso_a@pxe.pie.cri.epita.net>
2015-09-29 22:05:26 +0200

    Add test numbering functionnality

--- dessert.h
+++ dessert.h
@@ -2,7 +2,8 @@
 # define DESSERT_H
 
 #define dessert(A) \
-    if(!(A)) { printf("[KO] - %s - %s:%d \n", #A, __FILE__, __LINE__); } \
-    else {printf("[OK] - %s - %s:%d \n", #A, __FILE__, __LINE__); } \
+    if (!(A)) { printf("[KO] - %s - %s:%d", #A, __FILE__, __LINE__); dessert_fail++;} \
+    else {printf("[OK] - %s - %s:%d", #A, __FILE__, __LINE__); dessert_succ++;} \
+    printf(" - test %d\n", dessert_fail + dessert_succ); \
 
 #endif /* DESSERT_H */
